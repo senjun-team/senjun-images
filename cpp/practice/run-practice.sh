11fee2a02bb89dfa1c7f173527f5cd0815128201
Microvenator <116944723+Microvenator@users.noreply.github.com>
2025-11-20 17:56:32 +0300

    C++. Pracitce & task color output (#26)
    Co-authored-by: mesozoic.drones <mesozoic.drones@gmail.com>

--- cpp/practice/run-practice.sh
+++ cpp/practice/run-practice.sh
@@ -29,25 +29,33 @@ cd /home/code_runner/practice/$f
 cp /home/code_runner/practice/ut.cppm /home/code_runner/practice/$f
 
 # configure project
-if ! ( CXX=clang++ timeout 30s cmake -Bbuild -Wno-dev -GNinja > /tmp/configure.txt ); then
+if ! ( timeout 30s cmake -Bbuild -Wno-dev -GNinja > /tmp/configure.txt ); then
    echo "Configure error"
    cat /tmp/configure.txt
    echo user_solution_error_f936a25e
    exit
 fi
 
-# build cpp project
-if ! ( timeout 40s cmake --build build/ -- -j4  > /tmp/build.txt ); then
-   echo "Build error"
-   cat /tmp/build.txt
+SET_COLOR=$(tput setaf 2) # Set text color to green
+RESET_ALL=$(tput sgr0)  # Reset all
+
+# --quiet option is for ninja to suppress build steps output
+if ! ( CLICOLOR_FORCE=1 timeout 40s cmake --build build/ -- -j4 --quiet > /tmp/compile.txt ); then
+   echo "${SET_COLOR}Compiler errors:${RESET_ALL}"
+   cat /tmp/compile.txt
    echo user_solution_error_f936a25e
    exit
 fi
 
+if [ -s /tmp/compile.txt ]; then
+   echo "${SET_COLOR}Compiler warnings:${RESET_ALL}"
+   cat /tmp/compile.txt
+   echo ""
+fi
 
 if [ ${run+x} ]; then
   # run cpp project
-  # TODO which name for project should use here?
+  echo "${SET_COLOR}Console output:${RESET_ALL}"
   if ! ( timeout 5s ./build/main $user_options); then
     echo "Code execution timeout"
     echo user_solution_error_f936a25e
@@ -62,6 +70,7 @@ fi
 # e.g: sh run.sh -f dir_name -t
 if [ ${test+x} ]; then
   echo user_code_ok_f936a25e
+  echo "${SET_COLOR}Tests output:${RESET_ALL}"
   if ! ( timeout 5s ./build/tests ); then
    echo "Tests execution timeout"
    echo tests_cases_error_f936a25e
